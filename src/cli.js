56dcaabb0d048c9e2f2475fe3924f645f03a51e7
MaximChern <realdrug2@mail.ru>
2020-09-08 15:43:19 +0300

    update

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const zaprosName = () => {
+  console.log('Wellcom to the Brain Games!');
   const Name = readlineSync.question('May I have your name?');
   console.log(`Hello,${Name}!`);
 };
